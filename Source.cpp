c0f9602b803393bca81263a2fa85171c8802baa2
yazeed-dyriah <79354827+yazeed-dyriah@users.noreply.github.com>
2022-03-08 23:14:07 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -4,42 +4,56 @@
 using namespace std;
 
 
-void ReadCodeFile(string fileName) {
+
+void ReadCodeFile(string eof) {
 
     string line;
 
     string linesArray = { NULL };
 
-    ifstream file(fileName);
-
-    if (file.is_open()) {
-        cout << "file is open\n";
-        while (!file.eof()) {
-            getline(file, line);
-            cout << line << endl;
-            linesArray.append(line);
-        }
-    }
-    else
-        cout << "file is not open";
-    file.close();
-
 }
 
-
-
-
-
- 
+int isMagicNumber(int n) {
+    int digit_sum = 0;
+    while (n > 0 || digit_sum > 9) {
+        if (n == 0) {
+            n = digit_sum;
+            digit_sum = 0;
+        }
+        digit_sum += n % 10;
+        n /= 10;
+    }
+    return (digit_sum == 1 || digit_sum == -1 || digit_sum == 0);
+}
 
 
 int main()
 {
-    ReadCodeFile("eof.txt");
 
-    return 0;
+    ifstream file("eof.txt", ios::in);
+    if (file.is_open())
+    {
+        cout << "file is open\n \n";
+    }
+    else
+        cout << "file is not open\n \n ";
+   
+    int count;
+    string input;
 
-
+    while (file >> input) {
+        if ((input >= "a" && input <= "z") || (input >= "A" && input <= "Z")) {
+            cout << "is a string \n ";
+        }
+        else {
+            count = stoi(input);
+            isMagicNumber(count);
+            if (isMagicNumber(count)) {
+                cout << count << " is magic number\n";
+            }
+            else {
+                cout << count << " is not magic number\n";
+            }
+        }
+    }
 }
-
-
